787671e8d3bdb1b457b19d6647f75ac0cef0ccea
unknown <sneha.jayashankara@gmail.com>
2020-07-23 19:32:12 +0530

    inital commit

---
+++ myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from math import factorial
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>welcome to app</h1>")
+def home(request):
+    return render(request,"myapp/home.html",{'name':'sneha j'})
+def fact(request,n):
+    n=int(n)
+    return HttpResponse("<h4>factorial is {}</h4>".format(factorial(n)))
+
